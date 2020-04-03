a790ce2283a8989a472cd753b39ff18f3083540c
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-03 19:22:11 -0400

    Employee.test.js tests pass

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -6,20 +6,29 @@ class Employee {
   }
 
     getName() {
-      console.log(`Name: ${this.name}`);
+      console.log(this.name);
+      return this.name;
     }
 
     getId() {
-      console.log(`ID: ${this.id}`);
+      console.log(this.id);
+      return this.id;
     }
 
     getEmail() {
-      console.log(`Email: ${this.email}`);
+      console.log(this.email);
+      return this.email;
     }
 
     getRole() {
-      console.log(`Role: Employee`)
+      console.log(`Employee`)
+      return "Employee";
     }
 }
 
 module.exports = Employee;
+
+// const Betty = new Employee("Betty", 34567, "Betty@gmail.com", "UConn");
+// console.log(Betty);
+// Betty.getName();
+// Betty.getRole();
