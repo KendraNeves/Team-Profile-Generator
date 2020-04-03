4ff86156673a0178282f5c002bf92f4c6bd7cb9f
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-03 13:13:30 -0400

    Added Manager constructor function

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber){
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+
+  getRole(){
+    console.log("Manager");
+  }
+}
+
+const Jon = new Manager("Jon", 12345, "random@SpeechGrammarList.com", 123456);
+console.log(Jon);
